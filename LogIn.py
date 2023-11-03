41502291966960f6acdc86225d84967ce652f756
dhespen <149745811+dhespen@users.noreply.github.com>
2023-11-02 21:57:48 -0400

    Create LogIn.py

---
+++ LogIn.py
@@ -0,0 +1,43 @@
+username = "Admin"
+password = "1234"
+contFull = "Yes"
+
+while contFull == "Yes":
+
+    userIn = input("Username: ")
+    passIn = input("Password: ")
+
+    if userIn == username and passIn == password:
+
+        userChange = input("Would you like to change your username? ")
+
+        if userChange == "Yes":
+
+            username = input("New Username: ")
+
+            print("Username successfully changed\n")
+
+            passChange = input("Would you like to change your password? ")
+
+            if passChange == "Yes":
+
+                password = input("New Password: ")
+
+                print("Password successfully changed\n\n")
+
+        else:
+            passChange = input("Would you like to change your password? ")
+
+            if passChange == "Yes":
+                password = input("New Password: ")
+
+                print("Password successfully changed\n\n")
+
+
+    cont = input("Would you like to log back out (Y) or shut down (N)? ")
+
+    if cont == "Y":
+        contFull = "Yes"
+
+    else:
+        exit()
